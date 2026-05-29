65ea3390444396227f961f23ad4d5e373dc801e5
Matthew <nexx42tv@gmail.com>
2026-05-29 20:25:01 +0100

    fix build error

---
+++ frontend/yugen/src/app/(app)/library/page_client.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import * as api from "@lib/api.local"
+
+import { MediaCardInfo, PageResponse } from "@shared/types"
+import { ReactNode, useEffect, useState } from "react"
+
+import MediaCard from "@comps/mediaCard"
+import PageContainer from "@comps/pageContainer"
+
+import "./page.css"
+import { useSearchParams } from "next/navigation"
+
+type LibraryGroup = "ContinueWatching" | "Downloaded" | "Watching" | "OnHold" | "Planning" | "Completed" | "Dropped";
+
+export default function () {
+    const searchParams = useSearchParams();
+    const pageSize = 56;
+
+    const [refresh, setRefresh] = useState<number>(0);
+
+    const [selectedGroup, setSelectedGroup] = useState<LibraryGroup>(() => {
+        const group = searchParams.get("group");
+
+        const validGroups: LibraryGroup[] = [
+            "ContinueWatching",
+            "Downloaded",
+            "Watching",
+            "OnHold",
+            "Planning",
+            "Completed",
+            "Dropped",
+        ];
+
+        if (group && validGroups.includes(group as LibraryGroup)) {
+            return group as LibraryGroup;
+        }
+
+        return "ContinueWatching";
+    });
+
+    const [currentPage, setCurrentPage] = useState<number>(() => {
+        const page = searchParams.get("page");
+
+        if (page) {
+            const parsed = Number.parseInt(page, 10);
+
+            if (!Number.isNaN(parsed) && parsed > 0) {
+                return parsed;
+            }
+        }
+
+        return 1;
+    });
+
+    useEffect(() => {
+        window.history.replaceState(null, "", selectedGroup != null ? `?group=${selectedGroup}&page=${currentPage}` : "");
+    }, [selectedGroup, currentPage])
+
+    const search = (): Promise<PageResponse<MediaCardInfo>> => api.library_Search(currentPage - 1, pageSize, selectedGroup);
+
+    const drawGroupBtn = (group: LibraryGroup, label: string | undefined = undefined): ReactNode => {
+        const callback = () => {
+            setCurrentPage(1);
+            setSelectedGroup(group);
+        }
+
+        return (<button className={group === selectedGroup ? "Selected" : ""} onClick={callback}>{label ?? group}</button>)
+    }
+
+    const drawCard = (inp: MediaCardInfo): ReactNode => {
+        return <MediaCard key={inp.aniListId} Card={inp} requestRefresh={() => setRefresh(refresh + 1)} />
+    }
+    return (
+        <div className="Library">
+            <div className="Library_Filters">
+                {drawGroupBtn("ContinueWatching", "Continue Watching")}
+                {drawGroupBtn("Downloaded")}
+                {drawGroupBtn("Watching")}
+                {drawGroupBtn("OnHold", "On-Hold")}
+                {drawGroupBtn("Planning")}
+                {drawGroupBtn("Completed")}
+                {drawGroupBtn("Dropped")}
+            </div>
+
+            <div className="Library_Items">
+                {<PageContainer search={search} currentPage={currentPage} setCurrentPage={setCurrentPage} pageSize={pageSize} drawElement={drawCard} track={[selectedGroup, refresh]} />}
+            </div>
+        </div >
+    )
+}
